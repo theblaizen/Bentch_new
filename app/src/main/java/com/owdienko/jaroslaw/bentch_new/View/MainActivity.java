5bc8a6c9cb9322d6f2daf7b5998c36ba3a7120c1
Jaroslaw <jaroslaw.owdienko@gmail.com>
2017-07-17 23:40:56 +0300

    first setup mvp

--- app/src/main/java/com/owdienko/jaroslaw/bentch_new/View/MainActivity.java
+++ app/src/main/java/com/owdienko/jaroslaw/bentch_new/View/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        setUpToolBar();
         presenter = new MainActivityPresenter(getApplication(), this);
         presenter.onCreate();
     }
